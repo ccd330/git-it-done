5c1884a47d3dc3bb4d5ff4c40432f0eb608a2572
claudia.davis <claudiacdavis@gmail.com>
2022-01-18 14:59:51 -0500

    test push

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,7 @@
 var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+  fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+    console.log("inside", response);
+  });
 };
+
+console.log("outside");
